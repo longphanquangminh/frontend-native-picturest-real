a5f372cd6526ea578976977e1f2ddac59bab0fe6
longphanquangminh <lordaizensousuke2001@gmail.com>
2024-02-10 14:59:05 +0700

    update avatar logic none

--- src/components/Profile/Avatar.js
+++ src/components/Profile/Avatar.js
@@ -3,12 +3,17 @@ import { colors } from "../../config/theme";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import placeholder from "../../../assets/images/profile.jpg";
+import { connect } from "react-redux";
 
-const Avatar = ({ uri, style, imgStyle, onPress, onButtonPress, showChangeButton = true, aviOnly = false, ...props }) => {
+const Avatar = ({ userInfo, uri, style, imgStyle, onPress, onButtonPress, showChangeButton = true, aviOnly = false, ...props }) => {
+  console.log(uri);
   return (
     <View style={[styles.container, { marginBottom: aviOnly ? 0 : 15 }, style]} {...props}>
       <TouchableOpacity onPress={onPress}>
-        <Image source={uri ? { uri } : placeholder} style={[styles.image, aviOnly && { height: 35, width: 35, borderWidth: 0 }, imgStyle]} />
+        <Image
+          source={userInfo && userInfo?.anhDaiDien && uri ? { uri } : placeholder}
+          style={[styles.image, aviOnly && { height: 35, width: 35, borderWidth: 0 }, imgStyle]}
+        />
 
         {!aviOnly && showChangeButton && (
           <TouchableOpacity style={styles.editButton} onPress={onButtonPress}>
@@ -42,4 +47,14 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Avatar;
+const mapStateToProps = state => ({
+  userInfo: state.user.userInfo,
+});
+
+const mapDispatchToProps = dispatch => ({
+  login: credentials => dispatch({ type: "LOGIN", payload: credentials }),
+  setPosted: booleanValue => dispatch({ type: "POST", payload: booleanValue }),
+  setLoading: booleanValue => dispatch({ type: "LOADING", payload: booleanValue }),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Avatar);
